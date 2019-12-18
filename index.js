f5593af063c648769e76ec244e2ff18c291e2b91
Oleksandr Zinchenko <alex@begame.nl>
2019-12-18 17:16:26 +0100

    revert

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { helpers } = require('./helpers')
 
 module.exports.helpers = helpers
 
-module.exports = config => {
+module.exports.generateSteps = config => {
   const { actions, targets, areas, paths } = config
 
   const header = `/* eslint-disable */
