05e5cdd2724d08e16a46c28b9f13e808d034481f
Oleksandr Zinchenko <alex@begame.nl>
2019-12-20 15:34:38 +0100

    refactor utils

--- utils.js
+++ utils.js
@@ -1,32 +1,31 @@
-const illegalFilenameCharactersRegExp = /[\<\>\:\"\/\\\|\?\*]/g
+const illegalFilenameCharactersRegExp = /[\<\>\:\"\/\\\|\?\*\s]/g
+const funcRegEx = name => new RegExp(`${name}\\(['"]([^'"]+)['"]\\)`, 'g')
+const xpathFunctions = [
+  {
+    name: 'has-class',
+    replace: 'contains(concat(" ", normalize-space(@class), " "), " $1 ")',
+    format: /contains\(concat\(\"\s\",\snormalize-space\(\@class\),\s\"\s\"\),/g
+  },
+  {
+    name: 'has-text',
+    replace: 'contains(text(), "$1")',
+    format: /contains\(text\(\),/g
+  },
+  {
+    name: 'text-is',
+    replace: 'normalize-space(text())="$1")',
+    format: /normalize-space\(text\(\)\)=/g
+  }
+]
 
 module.exports = {
-  extendXpath: xpath => {
-    const funcWithStringArgRegEx = name => new RegExp(`${name}\\(['"]([^'"]+)['"]\\)`, 'g')
-    const rules = [
-      {
-        match: funcWithStringArgRegEx('has-class'),
-        replace: 'contains(concat(" ", normalize-space(@class), " "), " $1 ")'
-      },
-      {
-        match: funcWithStringArgRegEx('has-text'),
-        replace: 'contains(text(), "$1")'
-      },
-      {
-        match: funcWithStringArgRegEx('text-is'),
-        replace: 'normalize-space(text())="$1")'
-      }
-    ]
-    return rules.reduce((result, rule) => result.replace(rule.match, rule.replace), xpath)
-  },
+  extendXpath: xpath =>
+    xpathFunctions.reduce((result, rule) => result.replace(funcRegEx(rule.name), rule.replace), xpath),
   xpathToFileName: xpath =>
-    xpath
+    xpathFunctions.reduce((result, rule) => result.replace(rule.format, `${rule.name}(`), xpath)
       .replace(/(\sand\s)/g, '_and_')
-      .replace(/[\s"'()[\],@]|(\/\/)/g, '')
-      .replace(/(containsconcatnormalize-spaceclass)/g, '_with_class_')
-      .replace(/(normalize-spacetext)/g, '_text')
-      .replace(/=|(__)|\//g, '_')
-      .replace(illegalFilenameCharactersRegExp, '_'),
+      .replace(/(\sor\s)/g, '_or_')
+      .replace(illegalFilenameCharactersRegExp, ''),
   lastElement: xpath => `(${xpath})[last()]`,
   elementAtPosition: (xpath, number) => `(${xpath})[${number}]`,
   illegalFilenameCharactersRegExp
